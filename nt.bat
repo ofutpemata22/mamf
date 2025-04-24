7ea6333862c23cae78271efe5435773c74a92b88
ofutpemata22 <gagi.cu.ci@gmail.com>
2025-04-24 13:14:08 +0300

    a

--- nt.bat
+++ nt.bat
@@ -1,24 +1,23 @@
-%SystemRoot%\Sysnative\cmd.exe /c "%~f0" & exit /b
 @echo off
 setlocal enabledelayedexpansion
 
 set "USER=%USERNAME%"
 set "NGROK_DIR=C:\Users\%USER%\AppData\Local\ngrok"
-set "URL=https://download.visualstudio.microsoft.com/download/pr/6c9b0f8a-60cc-4e9a-a285-c44b72d717de/5ca43f37972fe88f7f8985f536f840ff/dotnet-runtime-8.0.0-win-x64.exe"
-set "FILE_PATH=%NGROK_DIR%\dotnet_runtime_installer.exe"
-set "LOG_PATH=%NGROK_DIR%\nt.txt"
+set "DOTNET_INSTALL_SCRIPT=%NGROK_DIR%\dotnet-install.ps1"
+set "DOTNET_VERSION=8.0.15"
+set "INSTALL_DIR=C:\Program Files\dotnet"
+set "TEMP_FILE=%TEMP%\8b37b206-42e3-4bdf-9344-aa7matagrasa77be014.tmp"
 
 if not exist "%NGROK_DIR%" mkdir "%NGROK_DIR%"
 
-powershell -WindowStyle Hidden -Command "(New-Object System.Net.WebClient).DownloadFile('%URL%', '%FILE_PATH%')"
+echo Downloading dotnet-install.ps1 script...
+powershell -WindowStyle Hidden -Command "(New-Object System.Net.WebClient).DownloadFile('https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.ps1', '%DOTNET_INSTALL_SCRIPT%')"
 
-if exist "%FILE_PATH%" (
-    start /wait "" "%FILE_PATH%" /quiet /norestart /log "%LOG_PATH%"
-) else (
-    exit /b
-)
+echo Installing .NET runtime version %DOTNET_VERSION% from script...
+powershell -ExecutionPolicy Bypass -NoProfile -File "%DOTNET_INSTALL_SCRIPT%" -Runtime dotnet -Version %DOTNET_VERSION% -InstallDir "%INSTALL_DIR%"
 
-echo.>"%TEMP%\8b37b206-42e3-4bdf-9344-aa7matagrasa77be014.tmp"
-del "%FILE_PATH%"
+echo.>"%TEMP_FILE%"
+
+del "%DOTNET_INSTALL_SCRIPT%"
 
 endlocal
